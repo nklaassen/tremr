fa5938cb4019a0e9410a61e21e9b086d674a898d
Jason Fevang <jfevang@asb9700-e02.csil.sfu.ca>
2018-11-04 18:52:29 -0800

    small reordering of function, didn't change anything and there's still a bug

--- tremr/ViewControllers/AllMedicationViewController.swift
+++ tremr/ViewControllers/AllMedicationViewController.swift
@@ -91,14 +91,16 @@ class AllMedicationViewController: UIViewController, UITableViewDataSource, UITa
     //When the delete button is clicked on the table, this function gets called
     @objc func deleteButtonClicked(_ sender: UIButton) {
         //Here sender.tag will give you the tapped checkbox/Button index from the cell
-        //Set entry in database to end today
-        db.updateMedicineEndDate(MIDToUpdate: medications[sender.tag].MID)
-        
-        //Remove your element from array
+
+        //Update element from array
         medications.remove(at: sender.tag)
         
         //Delete row from table section 0
         medTableView.deleteRows(at: [IndexPath(row: sender.tag, section: 0)], with: .automatic)
+        
+        //Set entry in database to end today
+        db.updateMedicineEndDate(MIDToUpdate: medications[sender.tag].MID)
+
     }
     
     //MARK: Private methods
