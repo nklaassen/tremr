3245e5b1771b94f5a67727b039162143b49af7b1
David Nishi Beckingham <davidnishibeckingham@d207-023-202-084.wireless.sfu.ca>
2018-11-14 16:28:33 -0800

    changed color of segmented control

--- tremr/ViewControllers/MainViewController.swift
+++ tremr/ViewControllers/MainViewController.swift
@@ -25,6 +25,12 @@ class MainViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         self.navigationController?.isNavigationBarHidden = true
+        //To change Navigation Bar Background Color
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.0, green: 0.6, blue: 1.0, alpha: 1.0)
+        //To change Back button title & icon color
+        UINavigationBar.appearance().tintColor = UIColor.white
+        //To change Navigation Bar Title Color
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
     }
 }
 
