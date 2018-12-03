b704732ce067c41a3f26f167f2648c2146a11835
Kira <knishibe@sfu.ca>
2018-12-02 18:48:49 -0800

    fixed ui for login 2

--- tremr/ViewControllers/LoginViewController.swift
+++ tremr/ViewControllers/LoginViewController.swift
@@ -21,8 +21,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController?.isNavigationBarHidden = false
-        self.navigationItem.title = "Login"
+        self.navigationController?.isNavigationBarHidden = true
         // Do any additional setup after loading the view.
         
         let tokenOptional = UserDefaults.standard.string(forKey: authTokenKey)
